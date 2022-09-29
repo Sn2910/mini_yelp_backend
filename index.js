984b1cf977f709bd421b8d94262675bf04aa5ca6
Sn2910 <sneha.shaw29@gmail.com>
2022-09-29 21:25:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
-const port = 8000;
+const port =  process.env.PORT || 8000;
 const restaurant={
     restaurant: {
         id:1,
