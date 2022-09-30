16dc651ab2af859b31a36e234477f7e46472acc5
Sn2910 <sneha.shaw29@gmail.com>
2022-09-30 10:56:21 +0200

    Update mongodb_operation.js

---
+++ controllers/mongodb_operation.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+const mongodbConnection = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/?retryWrites=true&w=majority`
+mongoose.connect(mongodbConnection)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection failed'))
